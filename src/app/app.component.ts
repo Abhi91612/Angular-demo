31eed6e188ee391b726caea8345e6fbede36e9b9
Abhishek Kumar <abhishek08310@gmail.com>
2026-01-13 12:33:22 +0530

    First

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TodoListItemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Demo';
+}
